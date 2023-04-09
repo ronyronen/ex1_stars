9cfe884bdb33eeea1d1ee5654ddd3c3ceda0a345
ronyronen <ronyr@simhawk.tech>
2023-04-09 10:56:16 +0300

    isomorphic

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ def list_images(path="."):
     return image_list
 
 
+def list_graphs(path="./output"):
+    graph_list = []
+    for file in os.listdir(path):
+        if file.endswith(".edgelist"):
+            # print(os.path.join("/.", file))
+            graph_list.append(os.path.join(path, file))
+    return graph_list
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     image_list  = list_images()
     for img_name in image_list:
-        scan_img = scan_image(img_name, threshold=190,s_min=10,s_max=250)
+        scan_img = scan_image(img_name, threshold=160,s_min=1,s_max=300)
+
+    graph_list = list_graphs()
+    matches = find_match(graph_list)
+    draw_matches()
 
 
     # images = np.concatenate((orig_img, scan_img), axis=1)
--- scan_photo.py
+++ scan_photo.py
@@ -1,15 +1,12 @@
+import os
+from path import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from datetime import datetime
-
-
-outline = np.array([
-    [-1, -1, -1],
-    [-1,  8, -1],
-    [-1, -1, -1]
-])
-
+from scipy.spatial import distance
+import networkx as nx
+import networkx.algorithms.isomorphism as iso
 
 def load_image(file_name: str):
     """
@@ -27,9 +24,11 @@ def scan_image(img_name, threshold=100, s_min=5, s_max=100):
     image = load_image(img_name)
     # make CSV file name from these params
     # time_stamp = datetime.now().strftime("%d%m%Y_%H%M%S")
-    log_name = img_name.strip("./").lower().split(".")[0]
-    logfile = open(f'{log_name}.csv', 'w')
-    logfile.write('y, x, r , b\n')
+    if not os.path.exists("./output"):
+        os.mkdir("./output")
+    image_name = img_name.strip("./").lower().split(".")[0]
+    logfile = open(f'./output/{image_name}.csv', 'w+')
+    logfile.write('i, x, y, r , b\n')
 
     # threshold
     th, th_img = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
@@ -37,6 +36,8 @@ def scan_image(img_name, threshold=100, s_min=5, s_max=100):
     contours = cv2.findContours(th_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
     # filter by area
+    i = 1
+    p = []
     for c in contours:
         area = cv2.contourArea(c)
         if s_min < area < s_max:
@@ -44,17 +45,118 @@ def scan_image(img_name, threshold=100, s_min=5, s_max=100):
             M = cv2.moments(c)
             cx = int(M["m10"] / M["m00"])
             cy = int(M["m01"] / M["m00"])
+            p.append((cx, cy))
 
             cv2.circle(th_img, (cx, cy), 40, (255, 0, 0), 3)
-            # cv2.putText(img, "Centre", (cx - 20, cy - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 1)
-            logfile.write(f'{cy}, {cx}, {area}, {image[cy,cx] / 255}\n')
+            cv2.putText(th_img, f'{i}', (cx - 20, cy - 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3)
+            logfile.write(f'{i}, {cx}, {cy}, {area}, {image[cy,cx] / 255}\n')
+            i += 1
+
+    create_mst(np.asarray(p), 750, image_name)
 
     # save images
     images = np.concatenate((image, th_img), axis=1)
-    cv2.imwrite(f'{log_name}_th.jpg', images)
+    cv2.imwrite(f'./output/{image_name}_th.jpg', images)
     # return the threshold image
     return th_img
 
+
+# def neighbors(data, image):
+#     tree = cKDTree(data=data)
+#     K = data.shape[0]
+#     results = tree.query(data, k=K)
+#     distances, indices = results
+#     n = 0
+#     index = indices[n]
+#     neighbours = data[index]
+#
+#     # plt.clf()
+#     # plt.scatter(data[1], data[0], color="red")
+#     # plt.scatter(neighbours[:, 1], neighbours[:, 0], color="blue")
+#     # plt.show()
+
+
+def create_mst(data, radius=1000, image_name="_"):
+    # distance matrix
+    dist = distance.cdist(data, data, 'euclidean')
+    # ignore diagonal values
+    np.fill_diagonal(dist, np.nan)
+    # extract i,j pairs where distance < threshold
+    # paires = np.argwhere(dist <= radius)
+    # groupby index
+    # tmp = np.unique(paires[:, 0], return_index=True)
+    # neighbors = np.split(paires[:, 1], tmp[1])[1:]
+    # indices = tmp[0]
+    G = nx.Graph()
+    for ind in range(len(data)):
+        paires = np.argwhere(dist <= radius)
+        # G.add_nodes_from(indices)
+        # G.add_edges_from(paires)
+        vxs = list()
+        edges = list()
+        for vx in paires:
+            if vx[0] != ind:
+                continue
+            vxs.append(vx[0]+1)
+            edges.append((vx[0]+1, vx[1]+1, int(dist[vx[0], vx[1]])))
+        G.add_nodes_from(vxs)
+        G.add_weighted_edges_from(edges)
+        T = nx.minimum_spanning_tree(G)
+        sorted(T.edges(data=True))
+
+        nx.write_weighted_edgelist(T, f'./output/T_{image_name}_{ind+1}.edgelist')
+
+        edge_labels = nx.get_edge_attributes(G, "weight")
+        # nx.draw(T, with_labels=True, font_weight='bold')
+        pos = nx.spring_layout(T, seed=0)
+        nx.draw(
+            T, pos, edge_color='black', width=1, linewidths=1,
+            node_size=500, node_color='pink', alpha=0.9,
+            labels={node: node for node in T.nodes()}
+        )
+        nx.draw_networkx_edge_labels(
+            T, pos,
+            edge_labels=edge_labels,
+            font_color='red'
+        )
+
+        # nx.draw_networkx_edge_labels(T, pos, edge_labels)
+        plt.savefig(f'./output/{image_name}_{ind+1}_T.jpg')
+        G.clear()
+        plt.clf()
+    # plt.show()
+
+
+def find_match(graph_list):
+    T = {}
+    for graph_name in graph_list:
+        f_name = Path(graph_name).name.splitext()[0].strip()
+        T[f_name] = nx.read_weighted_edgelist(graph_name)
+
+    key1 = '3046'
+    key2 = '3047'
+    T1, T2 = {}, {}
+    for key in T:
+        if key1 in key:
+            T1[key] = (T[key])
+        else:
+            T2[key] = (T[key])
+
+    # em = iso.categorical_edge_match('weight', 'weight')
+    em = lambda x,y: abs(x['weight'] - y['weight']) < 100
+    matches = []
+    for k1 in T1:
+        for k2 in T2:
+            if nx.is_isomorphic(T1[k1], T2[k2], edge_match=em):  # match weights
+            # if nx.is_isomorphic(T1[k1], T2[k2]):
+                matches.append((k1, k2))
+                print(f'isomorphic: {k1}, {k2}')
+    return matches
+
+
+def draw_matches(matches):
+    im1 = load_image('img_3047_th.jpg')
+    im2 = load_image('img_3047_th.jpg')
 
 def find_local_max(image):
     h = image.shape[0]
